275feada359e7789551d92102e7d27cb1008f7c6
Isaac Thill <ithill22@turing.edu>
2023-01-03 14:40:07 -0700

    Add reassignments and puts

--- variables.rb
+++ variables.rb
@@ -1,10 +1,16 @@
 starting_age = 37
+starting_age # 37
+starting_age = 33
 number_of_bananas = 17
 age_of_death = 83
 
 email_address = "helloworld1@gmail.com"
+email_address # "helloworld1@gmail.com"
+email_address = "goodbyeworld@gmail.com"
 my_string = "this is my string"
 worst_nfl_team = "minnesota vikings"
+worst_nfl_team # "minnesota vikings"
+worst_nfl_team = "Chicago Bears"
 
 deposit_paid = true
 eligible_to_play = false
@@ -14,3 +20,8 @@ amount_paid = 1200.50
 amount_owed = 1000.50
 money_found = 20.75
 
+puts "My starting age is #{starting_age}"
+puts "I ate #{number_of_bananas} bananas"
+puts "I hate the #{worst_nfl_team}"
+puts "Who's string is this? #{my_string}"
+puts "my email is #{email_address}"
